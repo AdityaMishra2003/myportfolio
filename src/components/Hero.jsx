f3f1d5f473a1b45f520a8dd7b5db89bf9abe32a9
AdityaMishra2003 <155647788+AdityaMishra2003@users.noreply.github.com>
2025-03-26 02:51:52 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,13 +14,13 @@ const Hero = () => {
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div>
+       <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Aditya</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I am a Software Developer, design <br className='sm:block hidden' />
+            backends and web applications
           </p>
         </div>
       </div>
